a4b5d7325fc7d3db483d90caf274c264dca61604
tahmina-nishu <akthertahmina572@gmail.com>
2025-02-23 09:18:50 +0600

    front() & back() function to access first and last element

---
+++ Vector/Front_and_Back_function.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {11, 23, 45, 9};
+
+    cout << "The front element is : " << v.front() << endl;
+    cout << "The back element is : " << v.back() << endl;
+
+
+    return 0;
+}
+
