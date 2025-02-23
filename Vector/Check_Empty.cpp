eef7ae4120bc4b895f7aede347b918cfed6c1e58
tahmina-nishu <akthertahmina572@gmail.com>
2025-02-23 09:09:09 +0600

    Check Empty

---
+++ Vector/Check_Empty.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {11, 23, 45, 9};
+    vector<int> v1 = {};
+
+    // Check if the vector (v) is empty
+    if (v.empty()) {
+        cout << "v is Empty" << endl;
+    } else {
+        cout << "v is not empty" << endl;
+    }
+
+    // Check if the vector (v1) is empty
+    if (v1.empty()) {
+        cout << "v1 is Empty" << endl;
+    } else {
+        cout << "v1 is not empty" << endl;
+    }
+
+    return 0;
+}
+
